0c6cfb468a1349490e158c8966bc8b4fd885e4bd
Seifer <seifer.ch@gmail.com>
2016-09-12 23:05:43 -0500

    Added hot deploy and some visual elements

--- src/main/java/com/seifernet/wissen/configuration/SecurityConfiguration.java
+++ src/main/java/com/seifernet/wissen/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
@@ -30,6 +31,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 					.permitAll( )
 				.and( )
 				.logout( )
+					.logoutRequestMatcher( new AntPathRequestMatcher( "/logout" ) )
+					.logoutSuccessUrl("/")
 					.permitAll( );
     }
 
