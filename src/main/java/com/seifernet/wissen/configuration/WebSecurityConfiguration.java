124f872de607633e99dbbff60f6555c6167b9c61
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2018-08-08 15:34:46 -0500

    Added error management and removed whitelabel

--- src/main/java/com/seifernet/wissen/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/seifernet/wissen/configuration/WebSecurityConfiguration.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.seifernet.wissen.service.CustomUserDetailsService;
@@ -31,7 +29,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(4);
+		return new BCryptPasswordEncoder();
 	}
 	
 	@Autowired
@@ -39,7 +37,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService)
+		auth
+			.userDetailsService(userDetailsService)
 			.passwordEncoder(passwordEncoder());
 	}
 
