5506b986c0b9d2bf8dd0e81b153e317a162884ba
Seiferson <seifer.ch@gmail.com>
2018-11-12 02:43:42 +0000

    Script for autofetching updates

---
+++ production_fetch.sh
@@ -0,0 +1,13 @@
+git remote update > /dev/null
+STATUS=$(git status -uno | grep "branch is" | cut -dw -f1 | cut -ds -f2)
+if [ "$STATUS" = " up-to-date " ]
+then
+	echo "Updated" >> /opt/app/wissen/auto.log
+else
+	git status -uno >> auto.log 
+	ps -ef | grep wissen | cut -d ' ' -f 6 | xargs kill -9
+	git pull
+	mvn clean spring-boot:run
+fi
+
+
---
+++ start.sh
@@ -0,0 +1,1 @@
+nohup mvn clean spring-boot:run &
