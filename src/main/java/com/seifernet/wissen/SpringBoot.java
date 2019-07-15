29ec868f3c12283102520a977ad3cb1bd1f5072c
seiferxx <seifer.ch@gmail.com>
2019-07-15 21:25:30 +0000

    Deleted RepositoryEventHandler and production_script.sh

--- src/main/java/com/seifernet/wissen/SpringBoot.java
+++ src/main/java/com/seifernet/wissen/SpringBoot.java
@@ -3,14 +3,12 @@ package com.seifernet.wissen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import com.seifernet.wissen.configuration.CustomProperties;
-import com.seifernet.wissen.repository.WissenRepositoryEventHandler;
 
 /**
  * Spring boot main class and annotation configuration
@@ -34,10 +32,4 @@ public class SpringBoot {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run( SpringBoot.class );
 	}
-	
-	@Bean
-    WissenRepositoryEventHandler repositoryEventHandler() {
-        return new WissenRepositoryEventHandler();
-    }
-
 }
--- src/main/java/com/seifernet/wissen/controller/WissenController.java
+++ src/main/java/com/seifernet/wissen/controller/WissenController.java
@@ -2,9 +2,12 @@ package com.seifernet.wissen.controller;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +28,9 @@ public class WissenController {
 	//private static final Logger logger = LoggerFactory.getLogger(WissenController.class);
 	
 	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
 	private AccountRepository repo;
 	
 	@PostMapping("/accounts/create")
@@ -37,6 +43,12 @@ public class WissenController {
     		Utils.isNotNullOrEmpty(account.getPassword()) &&
     		Utils.isNotNullOrEmpty(account.getNickname())
     	) {
+    		ArrayList<String> authorities = new ArrayList<String>();
+			authorities.add("USER");
+		
+			account.setEnabled(true);
+			account.setPassword(passwordEncoder.encode(account.getPassword()));
+			account.setAuthorities(authorities);
     		account = repo.insert(account);
     		
     		return ResponseEntity.ok(message);
