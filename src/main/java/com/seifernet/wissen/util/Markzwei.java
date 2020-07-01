3742622f06ca3f4126f7862fa2f086587fd33dc8
seiferxx <seifer.ch@gmail.com>
2020-06-30 22:33:40 -0500

    MISC changes related to javascript experimentation

---
+++ src/main/java/com/seifernet/wissen/util/Markzwei.java
@@ -0,0 +1,52 @@
+package com.seifernet.wissen.util;
+
+import java.util.Random;
+
+public class Markzwei {
+
+    public static String testString = "# Markzvei: a #Markdown alternative by @seiferson\n" +
+            "\n" +
+            "Markzvei is a markup language that translates to HTML using specific rules which\n" +
+            "differ from markdown by adding strict rules, you can follow this [link](http://link.com)\n" +
+            "\n" +
+            "There are multiple elements that can be used such as:\n" +
+            "  + Lists\n" +
+            "  * Emphasis elements\n" +
+            "  - Other stuff like blockquote,\n" +
+            "    links and images\n" +
+            "\n" +
+            "## Other important stuff\n" +
+            "\n" +
+            "This documents can also contain images ![image](http://image.com) and @user mentions\n" +
+            "and #king search tags\n" +
+            "\n" +
+            "```\n" +
+            "#Code blocks can be placed within backticks\n" +
+            "print('something')\n" +
+            "```\n" +
+            "\n" +
+            "Reasons to use markzvei:\n" +
+            "  1. Rules are well defined\n" +
+            "  2. no\n" +
+            "    lazy\n" +
+            "    definitions\n" +
+            "\n" +
+            "                      * * *\n" +
+            "\n" +
+            "> One day I will finish this project\n" +
+            "> Seiferson 2017\n" +
+            "\n" +
+            "Last but not least\n" +
+            "=====================================\n" +
+            "\n" +
+            "This is a last comment to close this document";
+
+
+    public static void fromHTMLtoMarkzwei(String text){
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
