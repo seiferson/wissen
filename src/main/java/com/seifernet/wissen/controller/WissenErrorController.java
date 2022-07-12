8579302109ce3cf2e45a8ccfbf520c29a985bb3c
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2022-07-12 11:08:47 -0500

    Deleted react code
    Moved to project wissen-ui
    https://github.com/seiferson/wissen-ui
    
    Added log message on wissen error controller

--- src/main/java/com/seifernet/wissen/controller/WissenErrorController.java
+++ src/main/java/com/seifernet/wissen/controller/WissenErrorController.java
@@ -20,7 +20,7 @@ public class WissenErrorController implements ErrorController {
     public String handleError(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-		logger.info(status.toString());
+		logger.info("HTTP Error " + status.toString());
 		if(status != null) {
 			Integer statusCode = Integer.valueOf(status.toString());
 			if(statusCode == HttpStatus.NOT_FOUND.value()){
